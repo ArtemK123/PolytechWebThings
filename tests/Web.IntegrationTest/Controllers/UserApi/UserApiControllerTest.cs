a8fed81ebeb742257a0e9dc7399d94c756d89322
ArtemK123 <darkmode1012@gmail.com>
2021-05-01 22:15:16 +0300

    Extracted UserApiProxy in integration tests

--- tests/Web.IntegrationTest/Controllers/UserApi/UserApiControllerTest.cs
+++ tests/Web.IntegrationTest/Controllers/UserApi/UserApiControllerTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Application.Users;
 using Application.Users.Commands.CreateUser;
@@ -11,10 +10,18 @@ using Domain.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
-namespace Web.IntegrationTest.Controllers
+namespace Web.IntegrationTest.Controllers.UserApi
 {
     internal class UserApiControllerTest : WebApiIntegrationTestBase
     {
+        private UserApiProxy userApiProxy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            userApiProxy = new UserApiProxy(HttpClient);
+        }
+
         [Test]
         public async Task Create_ValidUser_ShouldCreateUser()
         {
@@ -22,10 +29,7 @@ namespace Web.IntegrationTest.Controllers
             const string password = "12345678";
             const UserRole role = UserRole.User;
 
-            HttpResponseMessage response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "api/UserApi/Create")
-            {
-                Content = JsonContent.Create(new CreateUserCommand { Email = email, Password = password, Role = role })
-            });
+            HttpResponseMessage response = await userApiProxy.CreateAsync(new CreateUserCommand { Email = email, Password = password, Role = role });
             IUserRepository userRepository = WebApplicationFactory.Services.GetService<IUserRepository>() ?? throw new NullReferenceException();
             IUser storedUser = await userRepository.GetByEmailAsync(email) ?? throw new NullReferenceException();
 
@@ -40,10 +44,7 @@ namespace Web.IntegrationTest.Controllers
         [Test]
         public async Task Create_InvalidRequest_ShouldReturnValidationMessages()
         {
-            HttpResponseMessage response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "api/UserApi/Create")
-            {
-                Content = JsonContent.Create(new CreateUserCommand { Email = string.Empty, Password = string.Empty })
-            });
+            HttpResponseMessage response = await userApiProxy.CreateAsync(new CreateUserCommand { Email = string.Empty, Password = string.Empty });
 
             string expectedResponseMessage = "Validation failed: " + Environment.NewLine +
                                              " -- Email: Email is required." + Environment.NewLine +
@@ -60,16 +61,11 @@ namespace Web.IntegrationTest.Controllers
             const string email = "test31232121@gmail.com";
             const string password = "12345678";
             const UserRole role = UserRole.User;
+            CreateUserCommand createUserCommand = new CreateUserCommand { Email = email, Password = password, Role = role };
 
-            await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "api/UserApi/Create")
-            {
-                Content = JsonContent.Create(new CreateUserCommand { Email = email, Password = password, Role = role })
-            });
+            await userApiProxy.CreateAsync(createUserCommand);
 
-            HttpResponseMessage response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "api/UserApi/Create")
-            {
-                Content = JsonContent.Create(new CreateUserCommand { Email = email, Password = password, Role = role })
-            });
+            HttpResponseMessage response = await userApiProxy.CreateAsync(createUserCommand);
 
             string actualResponseMessage = await response.Content.ReadAsStringAsync();
 
---
+++ tests/Web.IntegrationTest/Controllers/UserApi/UserApiProxy.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Application.Users.Commands.CreateUser;
+
+namespace Web.IntegrationTest.Controllers.UserApi
+{
+    internal class UserApiProxy
+    {
+        private readonly HttpClient httpClient;
+
+        public UserApiProxy(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseMessage> CreateAsync(CreateUserCommand createUserCommand)
+        {
+            return await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Post, "api/UserApi/Create")
+            {
+                Content = JsonContent.Create(createUserCommand)
+            });
+        }
+    }
+}
