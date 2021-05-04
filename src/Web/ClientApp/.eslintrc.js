2793e8926bed53889afa33957023110c0ff294e1
ArtemK123 <darkmode1012@gmail.com>
2021-05-04 19:49:22 +0300

    Added eslint support to clientapp

---
+++ src/Web/ClientApp/.eslintrc.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+    extends: ["airbnb-typescript/base"],
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    rules: {
+        "linebreak-style": ["warn", "windows"],
+        "indent": ["warn", 4],
+        "@typescript-eslint/indent": ["warn", 4],
+        "quotes": ["warn", "double"],
+        "@typescript-eslint/quotes": ["warn", "double"],
+        "eol-last": ["warn", "never"],
+    }
+};
--- src/Web/ClientApp/webpack.config.js
+++ src/Web/ClientApp/webpack.config.js
@@ -1,49 +1,53 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-console.log('Using NODE_ENV = ' + process.env.NODE_ENV);
+const isDevelopment = process.env.NODE_ENV !== "production";
+console.log("Using NODE_ENV = " + process.env.NODE_ENV);
 
 module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  mode: isDevelopment ? "development" : "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"]
       },
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Polytech WebThings',
-      favicon: './src/assets/favicon.ico'
+      title: "Polytech WebThings",
+      favicon: "./src/assets/favicon.ico"
     }),
+    new ESLintPlugin({
+      extensions: ["ts"]
+    })
   ],
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true
   },
   optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+          name: "vendors",
+          chunks: "all",
         },
       },
     },
