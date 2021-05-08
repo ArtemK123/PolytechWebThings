74cf657c7f6accb1b659b7610433ce6fcc15b69b
ArtemK123 <darkmode1012@gmail.com>
2021-05-08 11:53:05 +0300

    Added support for scss (with inlining to sources)

--- src/Web/ClientApp/webpack.config.js
+++ src/Web/ClientApp/webpack.config.js
@@ -21,10 +21,21 @@ module.exports = {
                 test: /\.html$/,
                 use: ["html-loader"],
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: [".ts", ".js", ".scss"],
     },
     plugins: [
         new HtmlWebpackPlugin({
