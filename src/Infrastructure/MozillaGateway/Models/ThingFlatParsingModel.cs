8ea758b7cd2423213fe6c5be7fc23bea2eb88dee
ArtemK123 <darkmode1012@gmail.com>
2021-05-13 17:29:39 +0300

    Connected parsing to domain entities

---
+++ src/Infrastructure/MozillaGateway/Models/ThingFlatParsingModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Domain.Entities.WebThingsGateway;
+
+namespace PolytechWebThings.Infrastructure.MozillaGateway.Models
+{
+     internal class ThingFlatParsingModel
+     {
+         public string Title { get; init; }
+
+         [JsonPropertyName("@type")]
+         public IReadOnlyCollection<string> Types { get; init; }
+
+         public string Description { get; init; }
+
+         public string Href { get; init; }
+
+         public string SelectedCapability { get; init; }
+
+         public string Id { get; init; }
+
+         public IReadOnlyCollection<Link> Links { get; init; }
+
+         public IReadOnlyDictionary<string, JsonElement> Properties { get; init; }
+     }
+}
--- src/Infrastructure/MozillaGateway/Providers/ThingsProvider.cs
+++ src/Infrastructure/MozillaGateway/Providers/ThingsProvider.cs
@@ -6,8 +6,7 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Application.MozillaGateway.Providers;
-using Domain.Entities.Common;
-using Domain.Entities.WebThingsGateway.Action;
+using Domain.Entities.WebThingsGateway.Properties;
 using Domain.Entities.WebThingsGateway.Things;
 using Domain.Entities.Workspace;
 using Domain.Exceptions;
@@ -29,70 +28,14 @@ namespace PolytechWebThings.Infrastructure.MozillaGateway.Providers
             this.httpClientFactory = httpClientFactory;
         }
 
-        // public async Task<IReadOnlyCollection<IThing>> GetAsync(IWorkspace workspace)
-        // {
-        //     try
-        //     {
-        //         IReadOnlyCollection<ThingParsingModel> parsedThings = await GetParsedThingModelsAsync(workspace);
-        //         IReadOnlyCollection<IThing> convertedThings = Convert(parsedThings);
-        //         return convertedThings;
-        //     }
-        //     catch (Exception exception)
-        //     {
-        //         throw new BrokenGatewayCommunicationException(exception);
-        //     }
-        // }
-        //
-        // private async Task<IReadOnlyCollection<ThingParsingModel>> GetParsedThingModelsAsync(IWorkspace workspace)
-        // {
-        //     HttpResponseMessage response = await SendRequestToGatewayAsync(workspace);
-        //     string responseText = await response.Content.ReadAsStringAsync();
-        //     return Deserialize(responseText);
-        // }
-        //
-        // private IReadOnlyCollection<IThing> Convert(IReadOnlyCollection<ThingParsingModel> parsingModels) => parsingModels.Select(Convert).ToList();
-        //
-        // private IThing Convert(ThingParsingModel thingParsingModel)
-        // {
-        //     // IReadOnlyCollection<IProperty> properties = thingParsingModel.Properties.Select(keyValuePair => Convert(keyValuePair.Value)).ToList();
-        //     IReadOnlyCollection<IProperty> properties = Array.Empty<IProperty>();
-        //     IReadOnlyCollection<IAction> actions = Array.Empty<IAction>(); // TODO: Find a way to parse actions as well
-        //
-        //     return thingFactory.Create(new ParsedThingCreationModel(title: thingParsingModel.Title, href: thingParsingModel.Href, properties, actions));
-        // }
-        //
-        // private IProperty Convert(PropertyParsingModel parsingModel)
-        // {
-        //     return propertyFactory.Create(new ParsedPropertyCreationModel(name: parsingModel.Name, value: parsingModel.Value, href: parsingModel.Links.First().Href));
-        // }
-        //
-        // private async Task<HttpResponseMessage> SendRequestToGatewayAsync(IWorkspace workspace)
-        // {
-        //     string thingsUrl = workspace.GatewayUrl + "/things";
-        //     return await httpClientFactory.CreateClient().SendAsync(new HttpRequestMessage(HttpMethod.Get, thingsUrl)
-        //     {
-        //         Headers =
-        //         {
-        //             Accept = { new MediaTypeWithQualityHeaderValue("application/json") },
-        //             Authorization = new AuthenticationHeaderValue("Bearer", workspace.AccessToken)
-        //         }
-        //     });
-        // }
-        //
-        // private IReadOnlyCollection<ThingParsingModel> Deserialize(string serializedText)
-        // {
-        //     IReadOnlyCollection<ThingParsingModel>? deserialized
-        //         = JsonSerializer.Deserialize<IReadOnlyCollection<ThingParsingModel>>(serializedText, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-        //     return deserialized ?? throw new NullReferenceException();
-        // }
         public async Task<IReadOnlyCollection<Thing>> GetAsync(IWorkspace workspace)
         {
             try
             {
                 HttpResponseMessage response = await SendRequestToGatewayAsync(workspace);
                 string responseText = await response.Content.ReadAsStringAsync();
-                var result = Deserialize(responseText);
-                return Array.Empty<Thing>();
+                IReadOnlyCollection<Thing> parsedThings = Deserialize(responseText);
+                return parsedThings;
             }
             catch (Exception exception)
             {
@@ -113,20 +56,40 @@ namespace PolytechWebThings.Infrastructure.MozillaGateway.Providers
             });
         }
 
-        private IReadOnlyCollection<ThingParsingModel> Deserialize(string serializedText)
+        private IReadOnlyCollection<Thing> Deserialize(string serializedText)
         {
-            IReadOnlyCollection<ThingFlatModel>? deserialized
-                = JsonSerializer.Deserialize<IReadOnlyCollection<ThingFlatModel>>(serializedText, jsonSerializerOptions);
+            IReadOnlyCollection<ThingFlatParsingModel> flatThingModels
+                = JsonSerializer.Deserialize<IReadOnlyCollection<ThingFlatParsingModel>>(serializedText, jsonSerializerOptions)
+                  ?? throw new NullReferenceException();
 
-            IReadOnlyCollection<PropertyModelBase> parsedProperties =
-                deserialized.SelectMany(thing => thing.Properties.Select(keyValuePair => DeserializeProperty(keyValuePair.Value))).ToArray();
-            return Array.Empty<ThingParsingModel>();
+            IReadOnlyCollection<Thing> parsedThings = flatThingModels.Select(ParseThing).ToList();
+
+            // IReadOnlyCollection<PropertyModelBase> parsedProperties =
+            //     flatThingModels.SelectMany(thing => thing.Properties.Select(keyValuePair => DeserializeProperty(keyValuePair.Value))).ToArray();
+            return parsedThings;
         }
 
-        private PropertyModelBase DeserializeProperty(JsonElement propertyJson)
+        private Thing ParseThing(ThingFlatParsingModel flatThingModel)
+        {
+            IReadOnlyCollection<Property> parsedProperties = flatThingModel.Properties.Select(keyValuePair => ParseProperty(keyValuePair.Value)).ToArray();
+
+            return new Thing
+            {
+                Title = flatThingModel.Title,
+                Types = flatThingModel.Types,
+                Description = flatThingModel.Description,
+                Href = flatThingModel.Href,
+                SelectedCapability = flatThingModel.SelectedCapability,
+                Id = flatThingModel.Id,
+                Links = flatThingModel.Links,
+                Properties = parsedProperties
+            };
+        }
+
+        private Property ParseProperty(JsonElement propertyJson)
         {
             string propertyValueType = propertyJson.GetProperty("type").GetString();
-            PropertyModelBase parsedModel = default;
+            Property parsedModel = default;
             if (propertyValueType == "boolean")
             {
                 parsedModel = JsonSerializer.Deserialize<BooleanProperty>(propertyJson.GetRawText(), jsonSerializerOptions);
@@ -149,7 +112,7 @@ namespace PolytechWebThings.Infrastructure.MozillaGateway.Providers
             }
             else if (propertyValueType == "integer")
             {
-                parsedModel = JsonSerializer.Deserialize<IntegerProperty>(propertyJson.GetRawText(), jsonSerializerOptions);
+                parsedModel = JsonSerializer.Deserialize<NumberProperty>(propertyJson.GetRawText(), jsonSerializerOptions);
             }
             else
             {
