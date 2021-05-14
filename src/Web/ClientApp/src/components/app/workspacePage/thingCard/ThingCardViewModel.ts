9846aedfade03f5e4cd03677b9ea9e51521d05a7
ArtemK123 <darkmode1012@gmail.com>
2021-05-14 23:11:16 +0300

    Temporary removed properties from thing list

--- src/Web/ClientApp/src/components/app/workspacePage/thingCard/ThingCardViewModel.ts
+++ src/Web/ClientApp/src/components/app/workspacePage/thingCard/ThingCardViewModel.ts
@@ -11,12 +11,4 @@ export class ThingCardViewModel implements IViewModel {
         this.title(params.model.title);
         this.properties(params.model.properties);
     }
-
-    public generatePropertyText(property: IPropertyApiModel) {
-        const propertyText: string = `${property.name}: ${property.value}`;
-        if (property.unit) {
-            return `${propertyText} ${property.unit}`;
-        }
-        return propertyText;
-    }
 }
