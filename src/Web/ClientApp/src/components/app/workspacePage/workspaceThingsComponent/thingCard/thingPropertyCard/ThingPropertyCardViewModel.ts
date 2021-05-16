6b030836c2c242cfba5e7338bdeebd6c5c399b74
ArtemK123 <darkmode1012@gmail.com>
2021-05-16 22:14:49 +0300

    Added support for read-only properties

--- src/Web/ClientApp/src/components/app/workspacePage/workspaceThingsComponent/thingCard/thingPropertyCard/ThingPropertyCardViewModel.ts
+++ src/Web/ClientApp/src/components/app/workspacePage/workspaceThingsComponent/thingCard/thingPropertyCard/ThingPropertyCardViewModel.ts
@@ -1,11 +1,14 @@
+import * as ko from "knockout";
 import { IViewModel } from "src/componentsRegistration/IViewModel";
 import { IThingPropertyCardParams } from "src/components/app/workspacePage/workspaceThingsComponent/thingCard/thingPropertyCard/IThingPropertyCardParams";
 import { IPropertyApiModel } from "src/backendApi/models/response/things/IPropertyApiModel";
 
 export class ThingPropertyCardViewModel implements IViewModel {
+    public readonly inputValue: ko.Observable<string> = ko.observable("");
     private readonly model: IPropertyApiModel;
 
     constructor(params: IThingPropertyCardParams) {
         this.model = params.model;
+        this.inputValue(this.model.value);
     }
 }
