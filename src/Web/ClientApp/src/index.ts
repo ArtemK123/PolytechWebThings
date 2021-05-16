14be412af5901c6a5c2db0a56e5256925f602e59
ArtemK123 <darkmode1012@gmail.com>
2021-05-16 17:18:19 +0300

    Added font awesome icons

--- src/Web/ClientApp/src/index.ts
+++ src/Web/ClientApp/src/index.ts
@@ -1,6 +1,10 @@
 import * as ko from "knockout";
 import { ComponentRegistration } from "./componentsRegistration/ComponentRegistration";
 import { AppViewModel } from "./components/app/AppViewModel";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
 function main() {
     new ComponentRegistration().registerBindings();
