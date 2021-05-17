2f5e5aee646b99acb67acb325603a1db52b7477b
ArtemK123 <darkmode1012@gmail.com>
2021-05-17 06:09:05 +0300

    Added ThingsApiController with empty action

---
+++ src/Web/Controllers/ThingsApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.OperationResults;
+using Web.Models.Things.Request;
+
+namespace Web.Controllers
+{
+    public class ThingsApiController : ControllerBase
+    {
+        private readonly ISender mediator;
+
+        public ThingsApiController(ISender mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public Task<OperationResult> ChangePropertyState([FromBody] ChangePropertyStateRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Web/Models/Things/Request/ChangePropertyStateRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Models.Things.Request
+{
+    public record ChangePropertyStateRequest
+    {
+        public int? WorkspaceId { get; init; }
+
+        public string? ThingId { get; init; }
+
+        public string? PropertyName { get; init; }
+
+        public string? NewPropertyValue { get; init; }
+    }
+}
