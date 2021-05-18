d6405c0260699ca85447f2174038d31437c17922
ArtemK123 <darkmode1012@gmail.com>
2021-05-18 20:19:33 +0300

    Made rules collapsive

--- src/Web/ClientApp/src/components/app/workspacePage/workspaceRulesComponent/ruleCard/RuleCardViewModel.ts
+++ src/Web/ClientApp/src/components/app/workspacePage/workspaceRulesComponent/ruleCard/RuleCardViewModel.ts
@@ -3,9 +3,14 @@ import { IViewModel } from "src/componentsRegistration/IViewModel";
 import { IRuleCardParams } from "src/components/app/workspacePage/workspaceRulesComponent/ruleCard/IRuleCardParams";
 
 export class RuleCardViewModel implements IViewModel {
+    public readonly isCollapsed: ko.Observable<boolean> = ko.observable(true);
     public readonly params: IRuleCardParams;
 
     constructor(params: IRuleCardParams) {
         this.params = params;
+    }
+
+    public changeCollapseState(): void {
+        this.isCollapsed(!this.isCollapsed());
     }
 }
