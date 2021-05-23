3eb14e3f9c27cee65ed25d785a5ea561a33ea85a
ArtemK123 <darkmode1012@gmail.com>
2021-05-23 21:54:09 +0300

    Extracted RulesApiTestBase

--- tests/Web.IntegrationTest/Controllers/RulesApiControllerTests/DeleteRuleApiTest.cs
+++ tests/Web.IntegrationTest/Controllers/RulesApiControllerTests/DeleteRuleApiTest.cs
@@ -1,49 +1,22 @@
 ﻿using System.Threading.Tasks;
-using Domain.Entities.Rule;
 using NUnit.Framework;
 using Web.Controllers;
-using Web.IntegrationTest.Controllers.CommonTestBases.MockedGatewayThingsApi;
-using Web.IntegrationTest.Utils.ApiClients;
-using Web.IntegrationTest.Utils.Parsers;
 using Web.Models.OperationResults;
 using Web.Models.Rules.Request;
 using Web.Models.Rules.Response;
-using Web.Models.Rules.Steps;
 
 namespace Web.IntegrationTest.Controllers.RulesApiControllerTests
 {
     [TestFixture(TestOf = typeof(RulesApiController))]
-    internal class DeleteRuleApiTest : MockedGatewayThingsApiTestBase
+    internal class DeleteRuleApiTest : RulesApiTestBase
     {
-        private RulesApiClient rulesApiClient;
-
-        private int ruleId;
-
-        private DeleteRuleRequest DeleteRequest => new DeleteRuleRequest { RuleId = ruleId };
-
-        private CreateRuleRequest CreateRuleRequest => new CreateRuleRequest
-        {
-            RuleName = "Rule1",
-            WorkspaceId = WorkspaceId,
-            Steps = new[]
-            {
-                new StepApiModel { ExecutionOrderPosition = 0, StepType = StepType.ChangeThingState, ThingId = ThingId, PropertyName = PropertyName, NewPropertyState = NewPropertyState }
-            }
-        };
-
-        [SetUp]
-        public async Task SetUp()
-        {
-            rulesApiClient = new RulesApiClient(HttpClient, new HttpResponseMessageParser());
-            OperationResult<CreateRuleResponse> result = await rulesApiClient.CreateAsync(CreateRuleRequest);
-            ruleId = result.Data.CreatedRuleId;
-        }
+        private DeleteRuleRequest DeleteRequest => new DeleteRuleRequest { RuleId = RuleId };
 
         [Test]
         public async Task Delete_UnauthorizedUser_ShouldReturnUnauthorizedResult()
         {
             await UserApiClient.LogoutAsync();
-            OperationResult result = await rulesApiClient.DeleteAsync(DeleteRequest);
+            OperationResult result = await RulesApiClient.DeleteAsync(DeleteRequest);
             Assert.AreEqual(OperationStatus.Unauthorized, result.Status);
         }
 
@@ -51,7 +24,7 @@ namespace Web.IntegrationTest.Controllers.RulesApiControllerTests
         public async Task Delete_InvalidRequestModel_ShouldReturnErrorMessage()
         {
             int invalidRuleId = -1;
-            OperationResult result = await rulesApiClient.DeleteAsync(new DeleteRuleRequest { RuleId = invalidRuleId });
+            OperationResult result = await RulesApiClient.DeleteAsync(new DeleteRuleRequest { RuleId = invalidRuleId });
             Assert.AreEqual(OperationStatus.Error, result.Status);
             Assert.AreEqual("{\"RuleId\":[\"Non-positive ids are not supported\"]}", result.Message);
         }
@@ -59,8 +32,8 @@ namespace Web.IntegrationTest.Controllers.RulesApiControllerTests
         [Test]
         public async Task Delete_RuleIsNotFound_ShouldReturnErrorMessage()
         {
-            int nonExistingRuleId = ruleId + 1;
-            OperationResult result = await rulesApiClient.DeleteAsync(new DeleteRuleRequest { RuleId = nonExistingRuleId });
+            int nonExistingRuleId = RuleId + 1;
+            OperationResult result = await RulesApiClient.DeleteAsync(new DeleteRuleRequest { RuleId = nonExistingRuleId });
             Assert.AreEqual(OperationStatus.Error, result.Status);
             Assert.AreEqual($"Can not find rule with id={nonExistingRuleId}", result.Message);
         }
@@ -69,16 +42,16 @@ namespace Web.IntegrationTest.Controllers.RulesApiControllerTests
         public async Task Delete_UserHasNotEnoughRights_ShouldReturnForbiddenResult()
         {
             await ChangeUserAsync();
-            OperationResult result = await rulesApiClient.DeleteAsync(DeleteRequest);
+            OperationResult result = await RulesApiClient.DeleteAsync(DeleteRequest);
             Assert.AreEqual(OperationStatus.Forbidden, result.Status);
         }
 
         [Test]
         public async Task Delete_Success_ShouldDeleteRule()
         {
-            OperationResult deleteResult = await rulesApiClient.DeleteAsync(DeleteRequest);
+            OperationResult deleteResult = await RulesApiClient.DeleteAsync(DeleteRequest);
 
-            OperationResult<GetAllFromWorkspaceResponse> getRulesResult = await rulesApiClient.GetAllFromWorkspaceAsync(new GetAllFromWorkspaceRequest { WorkspaceId = WorkspaceId });
+            OperationResult<GetAllFromWorkspaceResponse> getRulesResult = await RulesApiClient.GetAllFromWorkspaceAsync(new GetAllFromWorkspaceRequest { WorkspaceId = WorkspaceId });
 
             Assert.AreEqual(OperationStatus.Success, deleteResult.Status);
             Assert.IsEmpty(getRulesResult.Data.Rules);
---
+++ tests/Web.IntegrationTest/Controllers/RulesApiControllerTests/RulesApiTestBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Domain.Entities.Rule;
+using NUnit.Framework;
+using Web.IntegrationTest.Controllers.CommonTestBases.MockedGatewayThingsApi;
+using Web.IntegrationTest.Utils.ApiClients;
+using Web.IntegrationTest.Utils.Parsers;
+using Web.Models.OperationResults;
+using Web.Models.Rules.Request;
+using Web.Models.Rules.Response;
+using Web.Models.Rules.Steps;
+
+namespace Web.IntegrationTest.Controllers.RulesApiControllerTests
+{
+    internal abstract class RulesApiTestBase : MockedGatewayThingsApiTestBase
+    {
+        protected RulesApiClient RulesApiClient { get; private set; }
+
+        protected int RuleId { get; private set; }
+
+        protected CreateRuleRequest CreateRuleRequest => new CreateRuleRequest
+        {
+            RuleName = "Rule1",
+            WorkspaceId = WorkspaceId,
+            Steps = new[]
+            {
+                new StepApiModel { ExecutionOrderPosition = 0, StepType = StepType.ChangeThingState, ThingId = ThingId, PropertyName = PropertyName, NewPropertyState = NewPropertyState }
+            }
+        };
+
+        [SetUp]
+        public async Task RulesApiTestBaseSetUp()
+        {
+            RulesApiClient = new RulesApiClient(HttpClient, new HttpResponseMessageParser());
+            OperationResult<CreateRuleResponse> result = await RulesApiClient.CreateAsync(CreateRuleRequest);
+            RuleId = result.Data.CreatedRuleId;
+        }
+    }
+}
