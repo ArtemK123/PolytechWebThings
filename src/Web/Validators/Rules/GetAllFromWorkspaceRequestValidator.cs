a0ec6f945b4cf00956621c09f52c7cc5a731403d
ArtemK123 <darkmode1012@gmail.com>
2021-05-23 17:18:38 +0300

    Added GetAllFromWorkspaceRequestValidator to source control

---
+++ src/Web/Validators/Rules/GetAllFromWorkspaceRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Web.Models.Rules.Request;
+using Web.Validators.Workspace;
+
+namespace Web.Validators.Rules
+{
+    public class GetAllFromWorkspaceRequestValidator : AbstractValidator<GetAllFromWorkspaceRequest>
+    {
+        public GetAllFromWorkspaceRequestValidator()
+        {
+            RuleFor(request => request.WorkspaceId).NotNull().SetValidator(new IntIdValidator());
+        }
+    }
+}
