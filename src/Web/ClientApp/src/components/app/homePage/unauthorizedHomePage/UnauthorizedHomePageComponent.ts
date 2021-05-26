50347844a3724e67da0769c50bb79bd383708c79
ArtemK123 <darkmode1012@gmail.com>
2021-05-26 23:00:41 +0300

    Improved UI for unauthorized home page

--- src/Web/ClientApp/src/components/app/homePage/unauthorizedHomePage/UnauthorizedHomePageComponent.ts
+++ src/Web/ClientApp/src/components/app/homePage/unauthorizedHomePage/UnauthorizedHomePageComponent.ts
@@ -2,6 +2,7 @@ import { IComponentDescriptor } from "src/componentsRegistration/IComponentDescr
 import { IComponent } from "src/componentsRegistration/IComponent";
 import { UnauthorizedHomePageViewModel } from "src/components/app/homePage/unauthorizedHomePage/UnauthorizedHomePageViewModel";
 import template from "./UnauthorizedHomePage.html";
+import "./UnauthorizedHomePage.scss";
 
 export class UnauthorizedHomePageComponent implements IComponent {
     public generateDescriptor(): IComponentDescriptor {
